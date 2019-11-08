c8a4eecd8de9f272a5a369ba64c5845db86855bd
Csaba Csuzdi <ccsuzdi@chemaxon.com>
2019-11-08 16:31:34 +0100

    CORS

--- backend/src/main/java/com/kalee/athenaeum/controller/DocumentController.java
+++ backend/src/main/java/com/kalee/athenaeum/controller/DocumentController.java
@@ -9,6 +9,7 @@ import com.kalee.athenaeum.data.S3Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class DocumentController {
 
