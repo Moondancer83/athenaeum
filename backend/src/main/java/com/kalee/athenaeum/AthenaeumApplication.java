3b59918b5b2e79e445b3e7b74de64dd022deab86
Csaba Csuzdi <ccsuzdi@chemaxon.com>
2019-11-08 15:24:37 +0100

    Add lambda handler

--- backend/src/main/java/com/kalee/athenaeum/AthenaeumApplication.java
+++ backend/src/main/java/com/kalee/athenaeum/AthenaeumApplication.java
@@ -7,12 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AthenaeumApplication {
-	private static final Logger LOG = LoggerFactory.getLogger(AthenaeumApplication.class);
 
-	public static void main(String[] args) {
-		LOG.info("--- Application is starting up ---");
-		SpringApplication.run(AthenaeumApplication.class, args);
-		LOG.info("--- Application has been started ---");
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(AthenaeumApplication.class);
+
+    public static void main(String[] args) {
+        LOG.info("--- Application is starting up ---");
+        SpringApplication.run(AthenaeumApplication.class, args);
+        LOG.info("--- Application has been started ---");
+    }
 
 }
